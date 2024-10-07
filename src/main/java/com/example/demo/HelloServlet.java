f37ef64f3085da0c23575efa6916ae4a9779dbb9
MalteAschenbach <malte.aschenbach@hogrefe.com>
2024-10-07 12:17:57 +0200

    different state across browsers

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -9,28 +9,19 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private int correct;
-    private int guess;
 
-    public void init() {
-        correct = (int) (Math.random() * 100.0);
-        guess = -1;
-    }
+    public void init() { }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        HttpSession session = request.getSession();
+        session.setAttribute("correct",(int) (Math.random() * 100.0));
+        session.setAttribute("guess",-1);
+
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1> Guessing Game </h1>");
-        if(guess != -1) {
-            out.println("Your last guess was: " + guess);
-            if (guess < correct) {
-                out.println("That guess was too low");
-            } else if (guess > correct) {
-                out.println("That guess was too high");
-            }
-        }
         out.println("<form action=\"/demo/hello-servlet\" method=\"post\"> <label for=\"guess\">Your Guess:</label> <input type=\"number\" name=\"guess\"> <input type=\"submit\"></form>");
         out.println("</body></html>");
     }
@@ -39,16 +30,21 @@ public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
+        HttpSession session = request.getSession();
+
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1> Guessing Game </h1>");
         try {
-            String guess_string = request.getParameter("guess").toString();
-           guess = Integer.parseInt(guess_string);
+            String guess_string = request.getParameter("guess");
+            session.setAttribute("guess", Integer.parseInt(guess_string));
         } catch (NumberFormatException e) {
             out.println("Guess has to be an integer");
         }
-        if(guess != -1) {
+        int guess = (int) session.getAttribute("guess");
+        int correct = (int) session.getAttribute("correct");
+
+        if(guess != -1 || true) {
             out.println("Your last guess was: " + guess + "<br>");
             if (guess < correct) {
                 out.println("That guess was too low");
